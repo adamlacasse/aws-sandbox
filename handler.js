b57ce43035ee1fea1734da44aa189027d6a85314
ADAM LACASSE <adam.lacasse@libertymutual.com>
2020-06-09 15:48:29 -0400

    WIP; initial files

---
+++ handler.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports.hello = async event => {
+  let userName = 'World';
+  if (event.queryStringParameters && event.queryStringParameters.name) {
+    userName = event.queryStringParameters.name
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: `Hello ${userName}! :poop:`,
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.shazbot = async event => {
+  let userName = 'World';
+  if (event.pathParameters && event.pathParameters.drunkle) {
+    userName = event.pathParameters.drunkle
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: `Fek ahf, ${userName}!`,
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
