f2be2f85a1feb85635ee2ef358e4afc3a538cdfc
ADAM LACASSE <adam.lacasse@libertymutual.com>
2020-06-09 17:20:30 -0400

    added cors to handler.hello

--- handler.js
+++ handler.js
@@ -7,6 +7,9 @@ module.exports.hello = async event => {
   }
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     body: JSON.stringify(
       {
         message: `Hello ${userName}! :poop:`,
